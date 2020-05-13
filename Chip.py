55918326605d78f3978bcc9d85610a8905e767c8
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-05-13 13:27:48 +0900

    initial commit

---
+++ Chip.py
@@ -0,0 +1,72 @@
+import pyxel
+
+
+class Chip:
+    # 画像バンクの各画像に関するクラス
+    # asset.pyxres の中身
+    # in image bank 0
+    #  [ 0][ 1][ 2][ 3]
+    #  [ 4][ 5][ 6][ 7]
+    #  [ 8][ 9][10][11]
+    #  [12][13][14][15]
+
+    ASSET_ID = {"pc_front": 0,
+                "pc_right": 1,
+                "pc_left": 2,
+                "pc_back": 3,
+                "npc_a": 4,
+                "npc_b": 5,
+                "npc_c": 6,
+                "item_a": 7,
+                "item_b": 8,
+                "item_c": 9,
+                "tile_grass": 10,
+                "tile_ground": 11,
+                "tile_sky": 12,
+                "wall_corner": 13,
+                "wall_edge": 14,
+                "wall_alone": 15}
+
+    # image bank の大きさ
+    IMGBANK_WIDTH = 4
+    IMGBANK_HEIGHT = 4
+
+    # 各画像の大きさ
+    IMG_WIDTH = 8
+    IMG_HEIGHT = 8
+
+    @classmethod
+    def draw_asset(cls, x: int, y: int, asset_id: int):
+        """ asset_id の画像を(x, y)に描画
+        """
+        assert 0 <= asset_id <= 15, "asset_id は0～15で指定"
+
+        u = (asset_id % cls.IMGBANK_WIDTH) * cls.IMG_WIDTH
+        v = (asset_id // cls.IMGBANK_WIDTH) * cls.IMG_HEIGHT
+
+        # asset はすべて白色(7)で透過
+        pyxel.blt(x, y, 0, u, v, cls.IMG_WIDTH, cls.IMG_HEIGHT, 7)
+
+    @classmethod
+    def is_wall(cls, asset_id):
+        no_collidables = [10, 11, 12]  # 背景
+        # 背景はfalse, それ以外はtrue
+        return asset_id not in no_collidables
+
+
+# 以下テスト用
+def update():
+    pass
+
+
+def draw():
+    pyxel.cls(0)
+    i = pyxel.frame_count//5 % 4
+    Chip.draw_asset(0, 0, i)
+    # print(Chip.is_wall(i))
+
+
+if __name__ == "__main__":
+    pyxel.init(80, 80)
+    pyxel.load("asset.pyxres")
+    pyxel.run(update, draw)
---
+++ Map.py
@@ -0,0 +1,104 @@
+from Chip import Chip
+import pyxel
+
+
+class Map:
+    SIZE = Chip.IMG_WIDTH  # size of each map chip
+    CHIP_WIDTH = Chip.IMG_WIDTH   # 4 map chips in a row
+    CHIP_HEIGHT = Chip.IMG_HEIGHT  # 4 map chips in a column
+
+    def __init__(self):
+        self.map = self.load_map("map.txt")
+        self.MAP_WIDTH = len(self.map[0])
+        self.MAP_HEIGHT = len(self.map)
+        # print(self.MAP_WIDTH, self.MAP_HEIGHT)
+
+    @classmethod
+    def to_screen(cls, i, j):
+        """map座標をscreen座標に変換
+        """
+        return (i * cls.SIZE, j * cls.SIZE)
+
+    @classmethod
+    def draw_chip(cls, i, j, asset_id):
+        """map座標(i, j)にasset_idのマップチップを描画
+        """
+        # Convert to screen coordinates of the chip image
+        x, y = cls.to_screen(i, j)
+        Chip.draw_asset(x, y, asset_id)
+
+    def draw_map(self):
+        for i in range(self.MAP_WIDTH):
+            for j in range(self.MAP_HEIGHT):
+                asset_id = self.map[j][i]
+                self.draw_chip(i, j, asset_id)
+
+    def load_map(self, txt):
+        map = []
+        map_file = open(txt)
+        # Read a line at a time.
+        for line in map_file:
+            array = []
+            data = line.split(",")
+            for d in data:
+                # Remove extra characters like \n
+                s = d.strip()
+                if s == "":
+                    break
+                v = int(d.strip())
+                array.append(v)
+            map.append(array)
+
+        return map
+
+    def set_map(self, i, j, asset_id):
+        # Sets a value at a specified position.
+        self.map[j][i] = asset_id
+
+    # Find a specific chip on the map
+    def search_map(self, val):
+        # Returns the coordinates where the given value exists.
+        for j, arr in enumerate(self.map):
+            for i, v in enumerate(arr):
+                # when found, return the coordinates
+                if v == val:
+                    return i, j
+        raise ValueError(f"val={val} is not found in a map.")
+
+    def is_wall(self, i, j):
+        # Check if a given coordinate is a wall
+        # outside the map
+        if i < 0 or self.MAP_WIDTH <= i:
+            return True
+        if j < 0 or self.MAP_HEIGHT <= j:
+            return True
+
+        # wall
+        v = self.map[j][i]
+        return Chip.is_wall(v)
+
+
+# テストして
+def update():
+    pass
+
+
+def draw():
+    pyxel.cls(12)
+    _map.draw_map()
+    is_wall = [[] for _ in range(_map.MAP_HEIGHT)]
+    for j in range(_map.MAP_HEIGHT):
+        for i in range(_map.MAP_WIDTH):
+            b = _map.is_wall(i, j)
+            is_wall[j].append(b)
+    # print(*is_wall, sep="\n")
+
+
+if __name__ == "__main__":
+    _map = Map()
+
+    pyxel.init(_map.MAP_WIDTH*Chip.IMG_WIDTH,
+               _map.MAP_HEIGHT*Chip.IMG_HEIGHT)
+    pyxel.load("asset.pyxres")
+
+    pyxel.run(update, draw)
